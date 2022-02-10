c2f8a075e4ddebfefb0800e1560993a672756a63
Gustavo <gordono@unsa.edu.pe>
2022-02-09 23:13:12 -0500

    Problem Solved with the Turtle Method

--- js/index.js
+++ js/index.js
@@ -1,23 +1,83 @@
 const filas = parseInt(prompt("Cantidad de Filas: "));
 const columnas = parseInt(prompt("Cantidad de Columnas: "));
 
-console.log(getNullMatrix(filas, columnas));
+let spiralMatrix = fillArray(filas, columnas);
+getSpiralMatrix(spiralMatrix);
+console.log(showArray(spiralMatrix));
 
-function getNullMatrix(rows, columns) {
+function fillArray(rows, columns) {
     arr = [];
     for(let i = 0; i < rows; i++) {
         arr.push([]);
         for(let j = 0; j < columns; j++) {
-            arr[i].push(0);
+            arr[i].push("Template");
         }
     }
     return arr;
 }
 
-/*
-function getSpiralMatrix(rows, columns) {
-    for(let i = 1; i <= rows * columns; i++) {
+function getSpiralMatrix(arr) {
+    const rows = arr.length;
+    const columns = arr[0].length;
 
+    const states = ["left", "down", "right", "up"];
+    let counter = 1, index = 0, actualState = states[index], i = 0, j = 0;
+
+    while(counter <= (rows * columns)) {
+        while(actualState === "left") {
+            if(!arr[i][j] || !(typeof(arr[i][j]) === "string")) {
+                actualState = states[++index % 4];
+                j--;
+                i++;
+                continue;
+            }
+            arr[i][j] = counter;
+            j++;
+            counter++;
+        }
+        while(actualState === "down") {
+            if(!arr[i] || !(typeof(arr[i][j]) === "string")) {
+                actualState = states[++index % 4];
+                i--;
+                j--;
+                continue;
+            }
+            arr[i][j] = counter;
+            i++;
+            counter++;
+        }
+        while(actualState === "right") {
+            if(!arr[i][j] || !(typeof(arr[i][j]) === "string")) {
+                actualState = states[++index % 4];
+                j++;
+                i--;
+                continue;
+            }
+            arr[i][j] = counter;
+            j--;
+            counter++;
+        }
+        while(actualState === "up") {
+            if(!arr[i] || !(typeof(arr[i][j]) === "string")) {
+                actualState = states[++index % 4];
+                j++;
+                i++;
+                continue;
+            }
+            arr[i][j] = counter;
+            i--;
+            counter++;
+        }
     }
 }
-*/
+
+function showArray(arr) {
+    let output = "";
+    for(let i = 0; i < arr.length; i++) {
+        for(let j = 0; j < arr[i].length; j++) {
+            output += arr[i][j] + "\t";
+        }
+        output += "\n";
+    }
+    return output;
+}
